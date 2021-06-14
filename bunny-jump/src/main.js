04644c79f6e6194b39201029a1f1ac85e23334f5
Reiya Odagaki <803204002@edu.cbe.ab.ca>
2021-06-14 13:10:04 -0600

    format

--- bunny-jump/src/main.js
+++ bunny-jump/src/main.js
@@ -1,22 +1,22 @@
-import Phaser from './lib/phaser.js'
+import Phaser from "./lib/phaser.js";
 
-console.dir(Phaser)
+console.dir(Phaser);
 
-import Game from './scenes/Game.js'
+import Game from "./scenes/Game.js";
 
 export default new Phaser.Game({
-     type: Phaser.AUTO,
-     width: 480,
-     height: 640,
-     scene: Game,
-     physics: {
-         default: 'arcade',
-         arcade: {
-         gravity: {
-         y: 200
-         },
-         debug: true
-         }
-        } 
-})
-    //PAGE 24 PDF / PAGE 21 PHASER
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  scene: Game,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 200
+      },
+      debug: true
+    }
+  }
+});
+//PAGE 24 PDF / PAGE 21 PHASER
--- bunny-jump/src/scenes/game.js
+++ bunny-jump/src/scenes/game.js
@@ -1,46 +1,47 @@
-import Phaser from '../lib/phaser.js'
+import Phaser from "../lib/phaser.js";
 
- export default class Game extends Phaser.Scene
- {
- constructor()
- {
- super('game')
- }
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
 
- preload()
- {
-    this.load.image('background', 'assets/bg_layer1.png')
-    this.load.image('platform', 'assets/ground_grass.png')
-    this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+  preload() {
+    this.load.image("background", "assets/bg_layer1.png");
+    this.load.image("platform", "assets/ground_grass.png");
+    this.load.image("bunny-stand", "assets/bunny1_stand.png");
+  }
 
- }
+  create() {
+    const platforms = this.physics.add.staticGroup();
 
- create()
- {
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
 
- const platforms = this.physics.add.staticGroup()
+      {
+        Phaser.Physics.Arcade.Sprite;
+      }
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
 
- 
- for (let i = 0; i < 5; ++i)
- {
- const x = Phaser.Math.Between(80, 400)
- const y = 150 * i
+      {
+        Phaser.Physics.Arcade.StaticBody;
+      }
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
 
+    const player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
 
- {Phaser.Physics.Arcade.Sprite}
- const platform = platforms.create(x, y, 'platform')
- platform.scale = 0.5
+    this.physics.add.collider(platforms, player);
+  }
+  update() {
+    const touchingDown = this.player.body.touching.down;
 
-
- {Phaser.Physics.Arcade.StaticBody}
- const body = platform.body
- body.updateFromGameObject()
- }
- 
- const player = this.physics.add.sprite(240, 320, 'bunny-stand')
- .setScale(0.5)
-
- this.physics.add.collider(platforms, player)
- }
-
- }
+    if (touchingDown) {
+      this.player.setVelocityY(-300);
+    }
+  }
+}
